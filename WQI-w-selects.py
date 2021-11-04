fb19f1c9b743d4cd7e7ff603f94a80b2f3c54b7b
amador2001 <amadorlopezhueros@gmail.com>
2021-11-04 10:50:22 +0100

    Update WQI-w-selects.py

--- WQI-w-selects.py
+++ WQI-w-selects.py
@@ -23,8 +23,151 @@ download_2 = requests.get(url_target).content
 target = pd.read_csv(io.StringIO(download_2.decode('utf-8')))
 
 # # dividimos ambos dataframes en sus sibconjuntos train and test
-#x_train, x_test, y_train, y_test = train_test_split(features, target, test_size=0.2, stratify=target)
+x_train, x_test, y_train, y_test = train_test_split(features, target, test_size=0.2, stratify=target)
 
 print(len(features))
 print(len(target))
+
+class StreamlitApp:
+
+    def __init__(self):
+
+        self.model = RandomForestClassifier()
+
+    def train_data(self):
+        self.model.fit(x_train, y_train)
+        return self.model
+
+    def construct_sidebar(self):
+
+        cols = [col for col in features.columns]
+
+        st.sidebar.markdown(
+            '<p class="header-style">Water Quality Prediction</p>',
+            unsafe_allow_html=True
+        )
+        valueFC = st.sidebar.selectbox(
+            f"Select {cols[0]}",
+            sorted(features[cols[0]].unique())
+        )
+
+        valueOxy = st.sidebar.selectbox(
+            f"Select {cols[1]}",
+            sorted(features[cols[1]].unique())
+        )
+
+        valuepH = st.sidebar.selectbox(
+            f"Select {cols[2]}",
+            sorted(features[cols[2]].unique())
+        )
+
+        valueTSS = st.sidebar.selectbox(
+            f"Select {cols[3]}",
+            sorted(features[cols[3]].unique())
+        )
+
+        valueTemperature = st.sidebar.selectbox(
+            f"Select {cols[4]}",
+            sorted(features[cols[4]].unique())
+        )
+
+        valueTPN = st.sidebar.selectbox(
+            f"Select {cols[5]}",
+            sorted(features[cols[5]].unique())
+        )
+
+        valueTP = st.sidebar.selectbox(
+            f"Select {cols[6]}",
+            sorted(features[cols[6]].unique())
+        )
+
+        valueTurb = st.sidebar.selectbox(
+            f"Select {cols[7]}",
+            sorted(features[cols[7]].unique())
+        )
+
+
+        #values = [sepal_length, sepal_width, petal_length, petal_width]
+        values = [[FC, Oxy, pH, TSS, Temperature, TPN, TP,Turb ]]
+
+        return values
+
+    def plot_pie_chart(self, probabilities):
+        fig = go.Figure(
+            data=[go.Pie(
+                    labels=list(iris_data.target_names),
+                    values=probabilities[0]
+            )]
+        )
+        fig = fig.update_traces(
+            hoverinfo='label+percent',
+            textinfo='value',
+            textfont_size=15
+        )
+        return fig
+
+    def construct_app(self):
+
+        self.train_data()
+        values = self.construct_sidebar()
+
+        values_to_predict = np.array(values).reshape(1, -1)
+
+        prediction = self.model.predict(values_to_predict)
+        prediction_str = iris_data.target_names[prediction[0]]
+        probabilities = self.model.predict_proba(values_to_predict)
+
+        st.markdown(
+            """
+            <style>
+            .header-style {
+                font-size:25px;
+                font-family:sans-serif;
+            }
+            </style>
+            """,
+            unsafe_allow_html=True
+        )
+
+        st.markdown(
+            """
+            <style>
+            .font-style {
+                font-size:20px;
+                font-family:sans-serif;
+            }
+            </style>
+            """,
+            unsafe_allow_html=True
+        )
+        st.markdown(
+            '<p class="header-style"> Iris Data Predictions </p>',
+            unsafe_allow_html=True
+        )
+
+        column_1, column_2 = st.beta_columns(2)
+        column_1.markdown(
+            f'<p class="font-style" >Prediction </p>',
+            unsafe_allow_html=True
+        )
+        column_1.write(f"{prediction_str}")
+
+        column_2.markdown(
+            '<p class="font-style" >Probability </p>',
+            unsafe_allow_html=True
+        )
+        column_2.write(f"{probabilities[0][prediction[0]]}")
+
+        fig = self.plot_pie_chart(probabilities)
+        st.markdown(
+            '<p class="font-style" >Probability Distribution</p>',
+            unsafe_allow_html=True
+        )
+        st.plotly_chart(fig, use_container_width=True)
+
+        return self
+
+
+sa = StreamlitApp()
+sa.construct_app()
 
